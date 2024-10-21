835aea9905dd041a5600c3d73629ebccd85303cb
Aditya khandelwal <khandelwal.aditya5@gmail.com>
2024-10-22 00:07:01 +0530

    3rd

--- Microservices using React course/posts/index.js
+++ Microservices using React course/posts/index.js
@@ -2,27 +2,35 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const crypto = require('crypto')
+const { default: axios } = require('axios')
 const app = express()
 const posts = {}
 
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/posts', (req,res) =>{
+app.get('/posts', (req, res) => {
     res.send(posts)
 })
 
-app.post('/posts', (req,res) =>{
+app.post('/posts', async (req, res) => {
     const id = crypto.randomBytes(4).toString('hex')
-    const {title} = req.body
+    const { title } = req.body
     posts[id] = {
         id, title
     }
+
+    await axios.post('http://localhost:4005/events', {
+        type: "PostCreated",
+        data: {
+            id, title
+        }
+    })
 
     res.status(201).send(posts[id])
 })
 
 
-app.listen(4000,()=> {
+app.listen(4000, () => {
     console.log('listen')
 })
